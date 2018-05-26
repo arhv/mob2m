1e7d41d17718025baee4c090ef47183bdab6de26
marcos <mhmiya@gmail.com>
2018-05-25 23:27:32 -0300

    Update User

--- src/main/resources/static/js/paisEstadoCidade.js
+++ src/main/resources/static/js/paisEstadoCidade.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$("#estado").change(function() {		
 		$.ajax({				
-				url: "http://localhost:8080/listStates/"+$("#estado").val(),
+				//url: "http://localhost:8080/listStates/"+$("#estado").val(),
+				url: "http://www.riomam.com.br/listStates/"+$("#estado").val(),
 				dataType: 'json',
 	            type: 'GET',
 	            data: {state : $('#state').val()},
