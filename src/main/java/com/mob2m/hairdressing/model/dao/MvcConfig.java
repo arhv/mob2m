80452d4ef138c49b415c0003d9249d2be1539e9f
marcos <mhmiya@gmail.com>
2018-07-01 22:19:47 -0300

    Update datasource

--- src/main/java/com/mob2m/hairdressing/model/dao/MvcConfig.java
+++ src/main/java/com/mob2m/hairdressing/model/dao/MvcConfig.java
@@ -18,13 +18,24 @@ public class MvcConfig { //extends WebMvcConfigurerAdapter {
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);*/
 	}
 
-	@Bean(name = "dataSource")
+	/*@Bean(name = "dataSource")
 	public DriverManagerDataSource dataSource() {
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 		driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
 		driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/hairdressing");
 		driverManagerDataSource.setUsername("hairdressingmaster");
 		driverManagerDataSource.setPassword("riomam150968");
+		return driverManagerDataSource;
+	}*/
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+		driverManagerDataSource.setUrl(
+				"//ruixtascnjatoe:6280b38ebd10415ca5023949d19ce1a4159a5a29128c240fa8bc653195afcb55@ec2-54-83-59-144.compute-1.amazonaws.com:5432/d1fe7d9ifvelh4");
+		driverManagerDataSource.setUsername("ruixtascnjatoe");
+		driverManagerDataSource.setPassword("6280b38ebd10415ca5023949d19ce1a4159a5a29128c240fa8bc653195afcb55");
 		return driverManagerDataSource;
 	}
 }
